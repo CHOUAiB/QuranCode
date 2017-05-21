3748712584d76ab84c9214655462e40a7882b3e5
Ali Adams <heliwave@yahoo.com>
2017-05-22 00:09:20 +0800

    -.git
     -.git

--- Version.bat
+++ Version.bat
@@ -1,8 +1,8 @@
 Tools\Replace\bin\Release\Replace.exe NET2 Version.bat 000 359
-Tools\Version\bin\Release\Version.exe .               6.19.359.4 6.19.359.4 -Tools
-Tools\Touch\bin\Release\Touch.exe Build               6:19                  -Tools
+Tools\Version\bin\Release\Version.exe .               6.19.359.4 6.19.359.4 -Tools -.git
+Tools\Touch\bin\Release\Touch.exe Build               6:19                  -Tools -.git
 Tools\Touch\bin\Release\Touch.exe Tools    2009-07-29 7:29
 COPY QuranCode1433.zip                QuranCode1433_6.19.359.4.zip
 COPY QuranCode1433.Source.zip         QuranCode1433_6.19.359.Source.zip
-Tools\Touch\bin\Release\Touch.exe .                   6:19                  -Tools
+Tools\Touch\bin\Release\Touch.exe .                   6:19                  -Tools -.git
 MOVE                               NET2\QuranCode1433_6.19.359.2.zip        .
