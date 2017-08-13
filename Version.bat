7d61ae2b772b25a58a59c661f98af2bc8f7124dc
Ali Adams <heliwave@yahoo.com>
2017-08-13 17:46:35 +0800

    QuranCode 6.19.439
    Updated root database.

--- Version.bat
+++ Version.bat
@@ -1,9 +1,9 @@
 IF not exist "NET2" NET2.bat
-Tools\Replace\bin\Release\Replace.exe NET2 Version.bat 000 433
-Tools\Version\bin\Release\Version.exe .               6.19.433.4 6.19.433.4 -Tools
+Tools\Replace\bin\Release\Replace.exe NET2 Version.bat 000 439
+Tools\Version\bin\Release\Version.exe .               6.19.439.4 6.19.439.4 -Tools
 Tools\Touch\bin\Release\Touch.exe Build               6:19                  -Tools
 Tools\Touch\bin\Release\Touch.exe Tools    2009-07-29 7:29
-COPY QuranCode1433.zip                QuranCode1433_6.19.433.4.zip
-COPY QuranCode1433.Source.zip         QuranCode1433_6.19.433.Source.zip
+COPY QuranCode1433.zip                QuranCode1433_6.19.439.4.zip
+COPY QuranCode1433.Source.zip         QuranCode1433_6.19.439.Source.zip
 Tools\Touch\bin\Release\Touch.exe .                   6:19                  -Tools
-MOVE                               NET2\QuranCode1433_6.19.433.2.zip        .
+MOVE                               NET2\QuranCode1433_6.19.439.2.zip        .
