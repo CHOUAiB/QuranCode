c48abf1b709e3849ec83bdd259fcba2e7b97ea21
Ali Adams <heliwave@yahoo.com>
2017-11-07 08:58:42 +0800

    QuranCode 6.19.487
    عذاب vs عذب

--- InitialLetters/Properties/AssemblyInfo.cs
+++ InitialLetters/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.19.479.4")]
-[assembly: AssemblyFileVersion("6.19.479.4")]
+[assembly: AssemblyVersion("6.19.487.4")]
+[assembly: AssemblyFileVersion("6.19.487.4")]
