d2b904e7df6cbb001593b47b62cb3228585964e4
Ali Adams <heliwave@yahoo.com>
2018-05-05 15:46:37 +0300

    QuranCode 6.19.617
    Updated 114.txt
    Added 114_506.txt
    Removed Primes.xls uses PurePrimes

--- Version.bat
+++ Version.bat
@@ -1,12 +1,12 @@
 IF not exist "NET2" NET2.bat
-Tools\Replace\bin\Release\Replace.exe NET2 Version.bat 000 613
-Tools\Version\bin\Release\Version.exe .               6.19.613.4 6.19.613.4 -Tools
+Tools\Replace\bin\Release\Replace.exe NET2 Version.bat 000 617
+Tools\Version\bin\Release\Version.exe .               6.19.617.4 6.19.617.4 -Tools
 Tools\Touch\bin\Release\Touch.exe Build               6:19                  -Tools
 Tools\Touch\bin\Release\Touch.exe Tools    2009-07-29 7:29
 MOVE                              NET2\QuranCode1433.2.zip                  .
 Tools\Touch\bin\Release\Touch.exe .                   6:19                  -Tools
-COPY QuranCode1433.zip        ..\Backup\QuranCode1433_6.19.613.4.zip
-COPY QuranCode1433.Source.zip ..\Backup\QuranCode1433_6.19.613.Source.zip
-COPY QuranCode1433.2.zip      ..\Backup\QuranCode1433_6.19.613.2.zip
+COPY QuranCode1433.zip        ..\Backup\QuranCode1433_6.19.617.4.zip
+COPY QuranCode1433.Source.zip ..\Backup\QuranCode1433_6.19.617.Source.zip
+COPY QuranCode1433.2.zip      ..\Backup\QuranCode1433_6.19.617.2.zip
 REN  QuranCode1433.zip                  QuranCode1433.Win10.zip
 REN  QuranCode1433.2.zip                QuranCode1433.Win7.zip
