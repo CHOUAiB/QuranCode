1660144212f545588394d09c589f343b5f9eb45d
heliwave <heliwave@yahoo.com>
2018-06-07 18:08:40 +0300

    QuranCode 6.19.643
    Al-Fatiha 7, 29 sample

--- Utilities/Subsets.cs
+++ Utilities/Subsets.cs
@@ -394,7 +394,7 @@ public class Subsets
             start_index--;
         }
 
-        // Find the first number in the sorted items that is the largest number we just found
+        // Find the first number in the sorted items that is the largest number we've just found
         // (in case of duplicates)
         while ((start_index > index) && (Items[start_index] == Items[start_index - 1]))
         {
@@ -672,25 +672,20 @@ public class Test
     }
     private static void Main(string[] args)
     {
-        long[] numbers = new long[]
-        {
-            7, 286, 200, 176, 120, 165, 206, 75, 129, 109,
-            123, 111, 43, 52, 99, 128, 111, 110, 98, 135,
-            112, 78, 118, 64, 77, 227, 93, 88, 69, 60,
-            34, 30, 73, 54, 45, 83, 182, 88, 75, 85,
-            54, 53, 89, 59, 37, 35, 38, 29, 18, 45,
-            60, 49, 62, 55, 78, 96, 29, 22, 24, 13,
-            14, 11, 11, 18, 12, 12, 30, 52, 52, 44,
-            28, 28, 20, 56, 40, 31, 50, 40, 46, 42,
-            29, 19, 36, 25, 22, 17, 19, 26, 30, 20,
-            15, 21, 11, 8, 8, 19, 5, 8, 8, 11,
-            11, 8, 3, 9, 5, 4, 7, 3, 6, 3,
-            5, 4, 5, 6
+        long[] fatiha_word_lengths = new long[] 
+        {   
+            3, 4, 6, 6,
+            5, 3, 2, 7,
+            6, 6,
+            3, 3, 5,
+            4, 4, 5, 6,
+            5, 5, 8,
+            3, 5, 5, 5, 3, 7, 5, 3, 7
         };
-        Subsets subsets = new Subsets(numbers);
+        Subsets subsets = new Subsets(fatiha_word_lengths);
 
         int count = 7;
-        long sum = 313;
+        long sum = 29;
         if (args.Length == 2)
         {
             int.TryParse(args[0], out count);
