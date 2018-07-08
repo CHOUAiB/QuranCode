e69a5e98749f97f6ddf3eed7692b7a1a80102b6f
heliwave <heliwave@yahoo.com>
2018-07-08 19:37:50 +0300

    QuranKey 6.19.707

---
+++ QuranKey/Version.bat
@@ -0,0 +1,11 @@
+IF not exist "NET2" NET2.bat
+Tools\Replace\bin\Release\Replace.exe NET2 Version.bat 000 701
+Tools\Version\bin\Release\Version.exe .               6.19.701.4 6.19.701.4 -Tools
+Tools\Touch\bin\Release\Touch.exe Build               6:19                  -Tools
+Tools\Touch\bin\Release\Touch.exe Tools                            2009-07-29 7:29
+ 
+Tools\Touch\bin\Release\Touch.exe .                   6:19                  -Tools
+COPY QuranKey.zip                  ..\Backup\QuranKey_6.19.701.zip
+COPY QuranKey.Source.zip           ..\Backup\QuranKey_6.19.701.Source.zip
+MOVE QuranKey.zip                  ..\Backup\
+MOVE QuranKey.Source.zip           ..\Backup\
