6d11b93b0a566ad945bdd32d51c51ad96ac4691c
Ali Adams <heliwave@yahoo.com>
2018-11-19 00:14:06 +0300

    QuranCode 6.19.757

--- Common/Clean.bat
+++ Common/Clean.bat
@@ -1,7 +1,13 @@
 @echo off
 :START
 
+del /F /S /Q *.tmp
+del /F /S /Q *.bak
 del /F /S /Q *.ini
+del /F /S /Q *.user
+del /F /S /Q *.pdb
+del /F /S /Q *.resources
+del /F /S /Q *.exe.config
 
 del /F /Q Translations\*.txt
 rd /S /Q Bookmarks
