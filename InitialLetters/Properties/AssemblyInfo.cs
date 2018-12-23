1ae30ed107889614509fe8971aabc0e68c2f2fb9
Ali Adams <heliwave@yahoo.com>
2018-12-23 20:11:50 +0300

    QuranCode 6.19.777
    Images added to README.md files :)

--- InitialLetters/Properties/AssemblyInfo.cs
+++ InitialLetters/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.19.777.4")]
-[assembly: AssemblyFileVersion("6.19.777.4")]
+[assembly: AssemblyVersion("6.19.787.4")]
+[assembly: AssemblyFileVersion("6.19.787.4")]
